bdd0e741b9e5302ff7bd8498cee4ad5400023a14
Eric Denis Laura <ericdenislaura@gmail.com>
2023-05-01 22:24:04 -0300

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-
+import Header from "./components/Header";
 function App() {
     return (
         <>
-            <h1>Control de gastos</h1>
+              <Header />
         </>
     );
 }
