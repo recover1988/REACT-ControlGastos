886b7cb88f85be2ba10bfbca78abc6846cdce600
Eric Denis Laura <ericdenislaura@gmail.com>
2023-05-01 22:32:21 -0300

    created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header";
 function App() {
+    const [presupuesto, setPresupuesto] = useState(0);
     return (
         <>
-              <Header />
+            <Header
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
         </>
     );
 }
