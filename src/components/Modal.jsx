9e2053f965d771febe5e907709d4eecfde931b1c
Eric Denis Laura <ericdenislaura@gmail.com>
2023-05-28 22:11:46 -0300

    modal elements

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -20,6 +20,44 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
             <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
                 <legend>Nuevo Gasto</legend>
+
+                <div className="campo">
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        placeholder="Añade el Nombre del Gasto"
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input
+                        type="number"
+                        id="cantidad"
+                        placeholder="Añade la Cantidad del Gasto: ej. 300"
+                    />
+                </div>
+                <div className="campo">
+                    <label htmlFor="categoria">Categoria</label>
+                    <select
+                        name=""
+                        id="categoria"
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">comida</option>
+                        <option value="casa">casa</option>
+                        <option value="gastos">gastos</option>
+                        <option value="ocio">ocio</option>
+                        <option value="salud">salud</option>
+                        <option value="suscripciones">suscripciones</option>
+                    </select>
+                    <input
+                        type="submit"
+                        value="Añadir Gasto"
+                    />
+                </div>
             </form>
         </div>
     );
